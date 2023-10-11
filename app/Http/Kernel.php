dcbc12df5ee6e4dcc26a6399a5d4a66e22ded354
Andrey Manzadey <andrey.manzadey@gmail.com>
2023-10-11 17:04:51 +0300

    remove HttpLogger

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,6 @@ class Kernel extends HttpKernel
         'api' => [
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \DragonCode\LaravelHttpLogger\Http\Middleware\HttpLogMiddleware::class,
         ],
 
         'api-admin' => [
--- routes/api/admin.php
+++ routes/api/admin.php
@@ -1,12 +1,6 @@
 <?php
 
-use App\Http\Controllers\API\Admin\HttpLogController;
 use App\Http\Controllers\API\Admin\TablesController;
 use Illuminate\Support\Facades\Route;
-
-Route::prefix('http-logs')->group(static function() {
-    Route::get('', [HttpLogController::class, 'index']);
-    Route::get('{httpLog}', [HttpLogController::class, 'show']);
-});
 
 Route::get('db/tables', TablesController::class);
