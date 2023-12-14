6778cb62bedef8f1db4d41c98e01ea6133367c8f
Andrey Manzadey <andrey.manzadey@gmail.com>
2023-12-14 20:05:41 +0300

    Update SiteUser.php

--- app/Models/SBuilder/SiteUser.php
+++ app/Models/SBuilder/SiteUser.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Models\SBuilder;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class SiteUser extends SBuilder
 {
     protected $table = 'sb_site_users';
@@ -13,4 +15,9 @@ class SiteUser extends SBuilder
     protected $casts = [
         'su_mail_status' => 'bool',
     ];
+
+    private function plugins(string $model) : HasMany
+    {
+        return $this->hasMany($model, 'p_user_id');
+    }
 }
